fdb50be20aa263b98a7097e0f074bc95ef9df43e
n00242247 <N00242247@iadt.ie>
2026-03-20 04:26:05 +0000

    created modules controller

---
+++ app/Http/Controllers/ModuleController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Module;
+use Illuminate\Http\Request;
+
+class ModuleController extends Controller
+{
+    public function index()
+    {
+        $modules = Module::all();
+        return view('modules.index', compact('modules'));
+    }
+
+    public function create()
+    {
+        return view('modules.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'code' => 'required|string|max:255',
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        Module::create($request->only('code', 'title', 'description'));
+
+        return redirect()->route('modules.index')->with('success', 'Module created.');
+    }
+
+    public function edit(Module $module)
+    {
+        return view('modules.edit', compact('module'));
+    }
+
+    public function update(Request $request, Module $module)
+    {
+        $request->validate([
+            'code' => 'required|string|max:255',
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $module->update($request->only('code', 'title', 'description'));
+
+        return redirect()->route('modules.index')->with('success', 'Module updated.');
+    }
+
+    public function destroy(Module $module)
+    {
+        $module->delete();
+
+        return redirect()->route('modules.index')->with('success', 'Module deleted.');
+    }
+}
