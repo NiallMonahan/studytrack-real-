d0f2e4845178acdf0436c046efe36a1ae6eb2549
N00242247 <N00242247@iadt.ie>
2026-04-24 19:34:13 +0100

    made the create

---
+++ resources/views/attendance/create.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Add Attendance
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <x-attendance-form :action="route('attendances.store')" :modules="$modules" />
+        </div>
+    </div>
+</x-app-layout>
