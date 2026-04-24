753aedff51c5eac68a9b24eea3a3b65f1313493a
N00242247 <N00242247@iadt.ie>
2026-04-24 18:54:31 +0100

    Created attendane mcr

---
+++ database/migrations/2026_04_24_175242_create_attendances_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('module_id')->constrained('modules')->onDelete('cascade');
+            $table->string('student_name');
+            $table->date('date');
+            $table->enum('status', ['present', 'absent', 'late']);
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
